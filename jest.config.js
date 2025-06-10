7755893eba67a2af0d036bd343d83cf3cdb9a2c6
adhiatmico <adhiatmico@gmail.com>
2025-06-10 16:04:19 +0700

    ignore file gapaerlu di test

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,20 @@
 // };
 
 
+// export default {
+//   testEnvironment: 'jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//   moduleNameMapper: {
+//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+//     '^@/(.*)$': '<rootDir>/src/$1'
+//   },
+//   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+//   }
+// };
+
+
 export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
@@ -15,7 +29,29 @@ export default {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/public/',
+    '<rootDir>/coverage/',
+    '<rootDir>/src/pages/_app.js',
+    '<rootDir>/src/pages/_document.js',
+    '<rootDir>/src/pages/api/',
+    '<rootDir>/src/pages/about.js',
+    '<rootDir>/src/pages/new.js',
+    '<rootDir>/src/pages/archives.js'
+  ],
+  collectCoverageFrom: [
+    'src/pages/**/*.{js,jsx,ts,tsx}',
+    'src/lib/**/*.{js,jsx,ts,tsx}',
+    '!src/pages/_*.js',
+    '!src/pages/api/**',
+    '!src/pages/about.js',
+    '!src/pages/new.js',
+    '!src/pages/archives.js',
+    '!**/*.config.js',
+    '!**/*.setup.js'
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   }
