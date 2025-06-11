13d19b476fa5c1916e0be673f90ea969c2e2ba62
adhiatmico <adhiatmico@gmail.com>
2025-06-11 20:33:08 +0700

    revisi dikit

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,  
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,  
     environment: process.env.NODE_ENV, 
     tracesSampleRate: 1.0, 
   });
